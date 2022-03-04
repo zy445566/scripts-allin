1b6bdc98201dc722d5b7c57d12dade678d19a35e
tomzhangyuan <tomzhangyuan@didiglobal.com>
2022-03-04 11:11:18 +0800

    fix: 升级关闭透传

--- index.js
+++ index.js
@@ -4,12 +4,22 @@ const commander = require('commander');
 const program = new commander.Command();
 
 function run(commandList) {
+    const childList = []
     for(const command of commandList) {
         const child = exec(command);
+        childList.push(child)
         child.stdout.pipe(process.stdout)
         child.stderr.pipe(process.stderr)
     }
-   
+    function closeChildHandle(signal) {
+        for(const child of childList) {
+            if(!child.killed) {
+                child.kill(signal)
+            }
+        }
+    }
+    process.on('SIGINT', closeChildHandle);
+    process.on('SIGTERM', closeChildHandle);
 }
 program.option('-s, --npm-script [script...]', 'npm script name of package.json');
 program.parse();
